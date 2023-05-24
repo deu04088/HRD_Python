abcbde7e4252da8c7b78342ba5a046da791a2290
Park <deu04088@naver.com>
2023-05-24 14:18:45 +0900

    5월 24일

--- ex07.py
+++ ex07.py
@@ -13,7 +13,7 @@ print(d, end = '\n\n')
 a = np.array([[1,2], [3, 4]])
 b = np.array([[10, 20], [30, 40]])
 
-# 각 위치에 따라 원소 간의 곱셈
+# 각 위치에 따라 원소 간의 곱셈, 일반 행렬곱과 결과가 다름
 print(a * b, end = '\n\n')  
 
 #일반적인 행렬곱
@@ -25,3 +25,12 @@ print(a @ b, end = '\n\n')
 c = np.arange(12)   # 0 ~ 11까지의 수열 저장
 print(c, end = '\n\n')
 print(c.reshape(3, 4), end = '\n\n')    # 3x4 행렬로 데이터 구조 변형
+
+# random
+rnd = np.random.randn(5) * 10 + 165     # 평균값이 165, 표준편차가 10인 값 생성
+print(rnd, end = '\n\n')
+
+# 인덱싱
+np_array = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]])
+print(np_array[np_array % 2 == 0])
+       
---
+++ 도전문제_3_6.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+# arange()를 사용하여 1 ~ 50까지의 원소를 가지는 다차원 배열을 만들자, 이 배열의 원소 
+# 50개를 랜덤하게 섞어 80%는 train_data에, 20%는 test_data에 넣어 이 두 배열을 반환
+# 하는 함수 train_test_split()을 만들자. 반환된 배열 값을 각각 출력하라.
+
+def train_test_split(data):
+    np.random.shuffle(data)
+
+    train_data = []
+    test_data = []
+
+    for i in range(1, len(data) + 1):
+        if (i / len(data) * 100) <= 80:
+            train_data.append(data[i-1])
+        else:
+            test_data.append(data[i-1])
+            
+    print(train_data, test_data, sep ='\n')
+
+# 아래 함수가 강사님이 짜신 파이썬에 가까운 함수
+# def train_test_split(data):
+#     np.random.shuffle(data)
+
+#     size = int(len(data)*0.8)
+#     train_data = data[:size]
+#     test_data = data[size:]
+            
+#     print(train_data, test_data, sep ='\n')
+    
+num = np.arange(1, 51)
+train_test_split(num)
+
+
